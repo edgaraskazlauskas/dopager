46a76a6dc29b16d6dde960278721a9db98cb58ad
Edgaras Kazlauskas <12482982+edgaraskazlauskas@users.noreply.github.com>
2018-05-16 17:55:10 +0300

    persist state

--- src/index.js
+++ src/index.js
@@ -1,24 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { PersistGate } from 'redux-persist/integration/react'
 import './index.css';
 import App from './components/app';
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
-import reducers from './features';
+import rootReducer from './features';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import registerServiceWorker from './registerServiceWorker';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web and AsyncStorage for react-native
 
+const persistedReducer = persistReducer({ key: 'root', storage }, rootReducer)
 const composeEnhancers = composeWithDevTools({
     // Specify name here, actionsBlacklist, actionsCreators and other options if needed
 });
-const store = createStore(reducers, /* preloadedState, */ composeEnhancers(
+const store = createStore(persistedReducer, /* preloadedState, */ composeEnhancers(
     applyMiddleware(thunk)
 ));
+const persistor = persistStore(store);
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+            <App />
+        </PersistGate>
     </Provider>,
     document.getElementById('root')
 );
