0cfa7f09d7ef7786b5b09e355d41e53e6cc45c25
Edgaras Kazlauskas <12482982+edgaraskazlauskas@users.noreply.github.com>
2018-05-17 08:44:30 +0300

    add vertical margins to pager

--- src/components/pager/text-pager.js
+++ src/components/pager/text-pager.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 const TextPager = ({ onClickedNext, onClickedPrevious }) => (
-    <div className="df jcsb c-gray">
+    <div className="df jcsb c-gray mvm">
         <div className="curp mhm" onClick={onClickedPrevious}>Previous Day</div>
         <div className="curp mhm" onClick={onClickedNext}>Next Day</div>
     </div>
